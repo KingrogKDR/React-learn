b7bc4e6135db5601bbae98294e4fdab0809bfb9b
KingrogKDR <abhisheksaikia766@gmail.com>
2024-01-19 18:48:49 +0530

    fragment tag added

--- react-nav-website/src/App.jsx
+++ react-nav-website/src/App.jsx
@@ -5,9 +5,11 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-400">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold text-blue-400">
+        Hello world!
+      </h1>
+    </>
   )
 }
 
