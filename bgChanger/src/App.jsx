6552bb2772c1f65355b53b10ed702c9ca27e8967
KingrogKDR <abhisheksaikia766@gmail.com>
2024-01-21 23:13:48 +0530

    dark mode banner changes applied

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className="w-full h-screen" style={{background: color}}>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 p-3">
-          <div className="flex flex-wrap justify-center bg-white px-4 py-2 rounded-3xl gap-3">
+          <div className="flex flex-wrap justify-center bg-white dark:bg-teal-400 px-4 py-2 rounded-3xl gap-3">
             <button className="bg-red-700 px-3 py-1 text-white rounded-2xl"
             onClick={() => setColor("rgb(185 28 28)")}>Red</button>
             <button className="bg-orange-500 px-3 py-1 text-white rounded-2xl"
